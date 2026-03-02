503dd7048981ac352c4d9d208e7252efcf1d19d3
AmadouSy <Amadousy941@gmail.com>
2026-03-02 21:23:49 +0000

    Initialize Spring Boot project

---
+++ src/test/java/com/Amadou/igotnext/IgotnextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Amadou.igotnext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgotnextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
